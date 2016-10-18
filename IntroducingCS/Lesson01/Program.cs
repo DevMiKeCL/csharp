d3b609a17712df43275f46258dd7d480065d978e
DevMiKeCL <MIGUEL.PERALTA@correoaiep.cl>
2016-10-18 00:31:53 -0300

    Condicion IF

--- IntroducingCS/Lesson01/Program.cs
+++ IntroducingCS/Lesson01/Program.cs
@@ -7,6 +7,15 @@ namespace Lesson01
         static void Main(string[] args)
         {
             Console.WriteLine("Hola Mundo");
+            int number1 = 10;
+            int number2 = 20;
+            if (number2 > number1)
+            {
+                Console.WriteLine("Numero 2 es mayor a Numero 1");
+            } else
+            {
+                Console.WriteLine("numero 1 es mayor a numero 2");
+            }
         }
     }
 }
